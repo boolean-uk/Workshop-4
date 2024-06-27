6fb205ebca90a14b1335e876f6b2890c171dd64f
Nazar Tymiv <nazartumiv@gmail.com>
2024-06-27 02:16:51 +0100

    feat: add method for get all recipes

--- recipesBook.ts
+++ recipesBook.ts
@@ -9,7 +9,7 @@ class Recipes {
     this._recipesList = []
   }
 
-  private addNew({ title, ingredients, instructions }: Recipe) {
+  public addNew({ title, ingredients, instructions }: Recipe) {
     const newRecipe = new Recipe({
       id: this._idCount,
       title,
@@ -21,16 +21,20 @@ class Recipes {
     this._idCount++
   }
 
-  private delete(recipeId: number) {
+  public delete(recipeId: number) {
     this._recipesList.filter(recipe => recipe.id !== recipeId)
   }
 
-  private update(recipeId: number, { title, ingredients, instructions }: Recipe) {
+  public update(recipeId: number, { title, ingredients, instructions }: Recipe) {
     const foundRecipe = this._recipesList.find(recipe => recipe.id === recipeId)
 
     if (foundRecipe) {
       foundRecipe.update({ id: foundRecipe.id, title, ingredients, instructions })
     }
+  }
+
+  public getAll(): Recipe[] {
+    return this._recipesList
   }
 
 }
