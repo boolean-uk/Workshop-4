50ae8f4f0d62d767dda73d2d5ece932bba118e2f
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2024-06-30 18:32:52 +0200

    basic setup of classes for Ingredient, Recipe, and Book

---
+++ classes.ts
@@ -0,0 +1,65 @@
+
+// Create a class Manager for all recipes
+// Methods to edit, delete, and display recipes
+
+type unit = "pcs" | "mL" | "L" | "g" | "kg" | "tbsp" | "tsp" | "cups" | "slices"
+
+export class Ingredient {
+	name: string
+	amount: number
+	unit: string
+
+	public constructor (name: string, amount: number, unit: unit) {
+		this.name = name
+		this.amount = amount
+		this.unit = unit
+	}
+}
+
+export class Recipe {
+	title: string
+	private instructions?: string
+	private duration?: number
+	private ingredientList?: Ingredient[]
+
+	public constructor (title: string, instructions?: string, duration?: number, ingredientList?: Ingredient[]) {
+		this.title = title
+		if (instructions)
+			this.instructions = instructions
+		if (duration)
+			this.duration = duration
+		if (ingredientList)
+			this.ingredientList = ingredientList
+	}
+
+	public describe(): string {
+		return (
+			`${this.title}
+Duration: ${this.duration} min
+Ingredients: ${this.ingredientList?.map(el => [el.amount, el.unit, el.name].join(" ")).join("\n")}
+Instructions: ${this.instructions}`
+		)
+	}
+}
+
+export class RecipeBook {
+	private readonly title: string;
+	private recipes: Recipe[];
+
+	public constructor (title: string) {
+		this.title = title;
+		this.recipes = []
+	}
+
+	public addRecipe (newRecipe: Recipe) {
+		this.recipes.push(newRecipe)
+	}
+
+	public findRecipe(title: string): string {
+		return this.recipes.find((recipe) => recipe.title === title)?.describe() || `No recipe titled ${title}.`
+	}
+
+	public listRecipes(): string {
+		return this.recipes.map((recipe, index) => `${index+1} ${recipe.title}`).join("\n")
+	}
+}
--- index.ts
+++ index.ts
@@ -2,3 +2,15 @@
 // Create a class Manager for all recipes
 // Methods to edit, delete, and display recipes
 
+import { Ingredient, Recipe, RecipeBook } from "./classes"
+
+const myEggs = new Ingredient("eggs", 2, "pcs")
+const myBread = new Ingredient("sourdough bread", 1, "pcs")
+const myTomatoes = new Ingredient("tomatoes", 1, "pcs")
+
+const myBreakfast = new Recipe("Eggs with bread", "make scrambled eggs, add chopped tomatoes, season, serve on bread", 15, [myBread, myEggs, myTomatoes])
+
+const myBook = new RecipeBook("My recipes")
+myBook.addRecipe(myBreakfast)
+console.log(myBook.listRecipes())
+console.log(myBook.findRecipe("Eggs with bread"))
