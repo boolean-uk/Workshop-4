4e6a347fba38633de118a0054ce373e127cdfa42
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2024-06-30 23:12:25 +0200

    simplify index.ts code

--- index.ts
+++ index.ts
@@ -14,19 +14,10 @@ const fruit = new Recipe("Fruit", "just eat an apple babe", 2, [])
 
 const myBook = new RecipeBook("My recipes")
 myBook.addRecipe(myBreakfast)
-myBook.addRecipe(myBreakfast)
 myBook.addRecipe(myBreakfast2)
 
 myBook.addRecipe(fruit)
 fruit.addIngredient(new Ingredient("apple", 1, "pcs"))
 fruit.addIngredient(new Ingredient("pear", 1, "pcs"))
 
-console.log(myBook.findRecipe("Eggs with bread"))
-myBreakfast.removeIngredient(new Ingredient("tomatoes", 1, "pcs"))
-console.log(myBook.findRecipe("Eggs with bread"))
-console.log(myBreakfast)
-
-myBook.deleteRecipe(-1)
-myBook.deleteRecipe(5)
-myBook.deleteRecipe(1)
 console.log(myBook.listRecipes())
