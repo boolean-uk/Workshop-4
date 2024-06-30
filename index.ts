53b89440fd31dca0de66af51d2ff15daabf0c3d2
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2024-06-30 18:49:21 +0200

    test out new method

--- index.ts
+++ index.ts
@@ -9,8 +9,16 @@ const myBread = new Ingredient("sourdough bread", 1, "pcs")
 const myTomatoes = new Ingredient("tomatoes", 1, "pcs")
 
 const myBreakfast = new Recipe("Eggs with bread", "make scrambled eggs, add chopped tomatoes, season, serve on bread", 15, [myBread, myEggs, myTomatoes])
+const myBreakfast2 = new Recipe("Cereal", "serve cereal and milk", 2, [])
+const fruit = new Recipe("Fruit", "just eat an apple babe", 2, [])
 
 const myBook = new RecipeBook("My recipes")
 myBook.addRecipe(myBreakfast)
-console.log(myBook.listRecipes())
 console.log(myBook.findRecipe("Eggs with bread"))
+myBook.addRecipe(myBreakfast)
+myBook.addRecipe(myBreakfast2)
+myBook.addRecipe(fruit)
+
+console.log(myBook.listRecipes())
+myBook.deleteRecipe(1)
+console.log(myBook.listRecipes())
