7e8ddd883855414b3d18d12dd64df6cbfd6a6a48
Lukas <lad_777@hotmail.com>
2024-07-03 16:11:35 +0100

    feat: addRecipe function added in new RecipeBook class

--- classes.ts
+++ classes.ts
@@ -1,14 +1,51 @@
 export class Recipe {
 
-    private readonly id: number;
-    private readonly title: string;
-    private readonly ingredients: string[];
-    private readonly instructions: string;
+    id: number;
+    title: string;
+    ingredients: string[];
+    instructions: string;
 
     public constructor(id: number, title: string, ingredients: string[], intructions: string) {
-        this.id = id,
-            this.title = title,
+        this.id = id
+        this.title = title,
             this.ingredients = ingredients,
             this.instructions = intructions
+    }
+
+    public getInstructions(): string {
+        return this.instructions
+    }
+
+    // Simple function to update information in recipe.
+    public updateRecipe(key: string, updateInfo: any) {
+        if (key === "title") {
+            this.title = updateInfo
+        } else if (key === "ingredients") {
+            this.ingredients = [updateInfo]
+        } else if (key === "instructions") {
+            this.instructions = updateInfo
+        } else {
+            return false
+        }
+    }
+
+    // A method to add items to ingredients
+    public addIngredient(newIngredient: string) {
+        this.ingredients.push(newIngredient)
+    }
+}
+
+export class RecipeBook {
+    recipes: Recipe[];
+
+    public constructor(recipes: Recipe[]) {
+        this.recipes = recipes
+    }
+
+    public addRecipe(recipeName: string, ingredients: string[], instructions: string) {
+        let id = this.recipes.length
+        let newRecipe = new Recipe(id, recipeName, ingredients, instructions)
+        this.recipes.push(newRecipe)
+        return newRecipe;
     }
 }
