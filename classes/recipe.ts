f64fb2337dec6fc1edd6e18ec97774a419fba6d4
Kye <NovusAa12@proton.me>
2024-07-13 15:39:36 +0100

    add Recipe class

--- classes/recipe.ts
+++ classes/recipe.ts
@@ -5,3 +5,15 @@ interface RecipeInterface {
   name: string;
   ingredients: Ingredient[];
 }
+
+export class Recipe implements RecipeInterface {
+  id: number;
+  name: string;
+  ingredients: Ingredient[];
+
+  constructor({ id, name, ingredients }: RecipeInterface) {
+    this.id = id;
+    this.name = name;
+    this.ingredients = ingredients;
+  }
+}
