e38a4dd119248f2b0a8b5dd3ceb9b0a4322ea442
Kye <NovusAa12@proton.me>
2024-07-15 22:41:15 +0100

    add addRecipe method to recipeBook class

--- classes/recipeBook.ts
+++ classes/recipeBook.ts
@@ -1,17 +1,51 @@
 import { Recipe } from "./recipe";
+import { Ingredient } from "../ingredient";
 
 class RecipeBook {
-  private readonly _title: string;
-  private _recipes: Recipe[];
-  private _recipeId: number = 0;
+  private readonly title: string;
+  private recipes: Recipe[];
+  private recipeId: number = 0;
 
   public constructor(title: string) {
-    this._title = title;
-    this._recipes = [];
+    this.title = title;
+    this.recipes = [];
   }
 
   public get allRecipes(): Recipe[] {
-    return this._recipes;
+    return this.recipes;
+  }
+
+  public addRecipe(
+    name: string,
+    ingredients: Ingredient[],
+    instructions: string
+  ): string {
+    if (ingredients.length === 0) {
+      return "Unable to add recipe: No ingredients added.";
+    }
+
+    if (!name || !instructions) {
+      return "Unable to add recipe: No name or instructions added.";
+    }
+
+    const isRecipeDuplicate = this.recipes.some(
+      (recipe) => recipe.name.toLowerCase() === name.toLowerCase()
+    );
+
+    if (isRecipeDuplicate) {
+      return "Unable to add recipe: A recipe with that name already exists.";
+    }
+
+    const recipeToAdd = new Recipe({
+      id: this.recipeId++,
+      name,
+      ingredients,
+      instructions,
+    });
+
+    this.recipes.push(recipeToAdd);
+
+    return `${name} recipe added to your recipe book: ${this.title}`;
   }
 }
 
