ca0b095e93d03b031b1f5dff8b5fa4377a01812b
Kye <NovusAa12@proton.me>
2024-07-15 22:35:52 +0100

    add instructions to Recipe

--- classes/recipe.ts
+++ classes/recipe.ts
@@ -4,26 +4,37 @@ interface RecipeInterface {
   id: number;
   name: string;
   ingredients: Ingredient[];
+  instructions: string;
 }
 
 export class Recipe implements RecipeInterface {
   id: number;
   name: string;
   ingredients: Ingredient[];
+  instructions: string;
 
-  constructor({ id, name, ingredients }: RecipeInterface) {
+  constructor({ id, name, ingredients, instructions }: RecipeInterface) {
     this.id = id;
     this.name = name;
     this.ingredients = ingredients;
+    this.instructions = instructions;
   }
 
-  public update({ name, ingredients }: Partial<RecipeInterface>): void {
+  public update({
+    name,
+    ingredients,
+    instructions,
+  }: Partial<RecipeInterface>): void {
     if (name) {
       this.name = name;
     }
 
     if (ingredients) {
       this.ingredients = [...this.ingredients, ...ingredients];
+    }
+
+    if (instructions) {
+      this.instructions = instructions;
     }
   }
 }
